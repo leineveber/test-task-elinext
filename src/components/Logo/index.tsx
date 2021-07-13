0ad68da368787db0b534824f6b3b93bf8cc65525
armistrice <armistrice@gmail.com>
2021-07-13 11:11:28 +0300

    made more styles for header and added roboto fonts

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 import { Link } from 'react-router-dom';
 
+import './Logo.styles.scss';
+
 const Logo: React.FC = (): React.ReactElement => {
   return (
-    <Link to='/'>
+    <Link className='logo' to='/'>
       <h1>Image Finder</h1>
     </Link>
   );
