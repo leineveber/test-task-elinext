c24621cb740439db27513bd81068e1744219158e
armistrice <armistrice@gmail.com>
2021-07-13 10:19:45 +0300

    initialize project

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,16 @@
+// react
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { store } from './app/store';
+
+// store
 import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
+import { store } from './redux/store';
+
+// components
+import App from './App';
+
+// styles
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -14,8 +20,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
